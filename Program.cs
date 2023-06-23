cea375a3bc14bdd19b113a9bc3114fb2301df060
Alex <aleksejtatukov098@gmail.com>
2023-06-23 22:53:33 +0300

    the last

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int N = Convert.ToInt16(Console.ReadLine());
+int m = 1;
+if (N < 0)
+{
+    Console.WriteLine("Nothing");
+}
+while (m <= N)
+{
+    if ( m % 2 == 0)
+ { 
+    Console.WriteLine(m);
+ }
+    m++;
+}
